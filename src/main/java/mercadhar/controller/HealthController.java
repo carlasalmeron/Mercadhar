846249b985060ae771b7ce05da7dade337ad513f
Carla Salmerón <salmeroncarla98@gmail.com>
2026-06-25 03:16:33 +0200

    feat: add root healthcheck endpoint

---
+++ src/main/java/mercadhar/controller/HealthController.java
@@ -0,0 +1,12 @@
+package mercadhar.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String healthCheck() {
+        return "¡Backend Mercadhar corriendo perfectamente!";
+    }
+}
